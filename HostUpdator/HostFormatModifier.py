8f1870bbec9d9e175d535f9e093f42622101e4fb
Charlie Hu <charlie.yaha@gmail.com>
2017-05-04 20:23:32 +0800

    python modified

--- HostUpdator/HostFormatModifier.py
+++ HostUpdator/HostFormatModifier.py
@@ -5,53 +5,47 @@ import sys
 import os
 
 
+class hostDecorator(object):
 
-rawHost = open("HostUpdator/hosts", "r")
-baseRules = open("HostUpdator/HostBaseRules.conf","r")
-ruleList = open("HostUpdator/Host-list.conf","wb+")
+    def __init__(self):
+        self.rawHost = open("HostUpdator/hosts", "r")
+        self.baseRules = open("HostUpdator/HostBaseRules.conf","r")
+        self.ruleList = open("HostUpdator/Host-list.conf","wb+")
+    
+    def HostAddressModified(self,LineString):
+        tempString = LineString.replace("\n","")
+        tempString = tempString.replace(" ","\t")
+        StringSplit = tempString.split("\t")
+        HostAddress = [StringSplit[0],StringSplit[-1]]
+        return HostAddress
+    
+    def HostAddressFormatforSRules(self,AddressList):
+        AddressIP = AddressList[0]
+        AddressDomain = AddressList[1]
+        AddressString = "%s = %s\n"%(AddressDomain,AddressIP)
+        return AddressString
 
+    
+    def HostAddrssUpdator(self):
+        rawHostLines = self.rawHost.readlines()
+        HostUpdateList = []
 
+        for line in rawHostLines[6:]:
+            line = self.HostAddressModified(line)
+            line = self.HostAddressFormatforSRules(line)
+            HostUpdateList.append(line)
 
+        baseLines = self.baseRules.readlines()
+        UpdatedList = baseLines + HostUpdateList
 
+        self.ruleList.writelines(UpdatedList)
 
+        self.rawHost.close()
+        self.ruleList.close()
 
-
-def HostAddressModified(LineString):
-    tempString = LineString.replace("\n","")
-    tempString = tempString.replace(" ","\t")
-    StringSplit = tempString.split("\t")
-    HostAddress = [StringSplit[0],StringSplit[-1]]
-    return HostAddress
-
-
-def HostAddressFormatforSRules(AddressList):
-    AddressIP = AddressList[0]
-    AddressDomain = AddressList[1]
-    AddressString = "%s = %s\n"%(AddressDomain,AddressIP)
-    return AddressString
-
-
-
-
-rawHostLines = rawHost.readlines()
-HostUpdateList = []
-
-for line in rawHostLines[6:]:
-    line = HostAddressModified(line)
-    line = HostAddressFormatforSRules(line)
-    HostUpdateList.append(line)
-
-# print HostUpdateList
-
-baseLines = baseRules.readlines()
-UpdatedList = baseLines + HostUpdateList
-
-# print UpdatedList
-
-ruleList.writelines(UpdatedList)
+        print ("done!")
 
 
-rawHost.close()
-ruleList.close()
 
-print ("done!")
+hostUpdate = hostDecorator()
+hostUpdate.HostAddrssUpdator()
